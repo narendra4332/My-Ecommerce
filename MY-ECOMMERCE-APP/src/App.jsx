65c7043e973aaf37659c85e8f8a2e8276644d5e6
Narendra <narendralodhi862@gmail.com>
2025-03-05 11:51:34 +0530

    add user signIn and signUp page

--- MY-ECOMMERCE-APP/src/App.jsx
+++ MY-ECOMMERCE-APP/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./Firebase";
+
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import ChecKout from "./Pages/ChecKout";
-import ThankYou from "./Pages/Thankyou";
+import ThankYou from "./Pages/ThankYou";
 import Header from "./Components/Header";
 import About from "./Pages/About";
 import ProductCard from "./Components/ProductCard";
 import Footer from "./Components/Footer";
+import Auth from "./Pages/Auth";
 
 function App() {
   const [cart, setCart] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
 
   const addToCart = (product) => {
-    setCart([...cart, product]); // Add the product to the cart
+    setCart([...cart, product]);
   };
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    setUser(null);
+  };
+
   return (
     <Router>
-      <Header cart={cart} />
+      {user && <Header cart={cart} handleLogout={handleLogout} />}
       <Routes>
-        <Route path="/" element={<ProductCard />} />
-        <Route path="About" element={<About />} />
-        <Route path="/Product" element={<Product addToCart={addToCart} />} />
-        <Route path="/Cart" element={<Cart cart={cart} />} />
-        <Route
-          path="/Checkout"
-          element={<ChecKout cart={cart} setCart={setCart} />}
-        />
-        <Route path="/ThankYou" element={<ThankYou />} />
+        {!user ? (
+          <>
+            <Route path="/auth" element={<Auth />} />
+            <Route path="*" element={<Navigate to="/auth" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<ProductCard />} />
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/Product"
+              element={<Product addToCart={addToCart} />}
+            />
+            <Route path="/cart" element={<Cart cart={cart} />} />
+            <Route
+              path="/checkout"
+              element={<ChecKout cart={cart} setCart={setCart} />}
+            />
+            <Route path="/thankyou" element={<ThankYou />} />
+          </>
+        )}
       </Routes>
-      <Footer />
+      {user && <Footer />}
     </Router>
   );
 }
